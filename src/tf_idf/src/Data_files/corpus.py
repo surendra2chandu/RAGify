a69910be5be38e71e5106bd4b1ca5e67a91ccfb0
chandu <surendra2chandu@gmail.com>
2025-01-09 17:06:37 +0530

    up

--- src/tf_idf/src/Data_files/corpus.py
+++
@@ -1,8 +0,0 @@
-
-documents = [
-    "This is the first document",
-    "This document is the second document",
-    "And this is the third one ",
-    "Is this the first document adi",
-    "Is this the first first document first adi narayana choudary reddy"
-]
