c5de8246ee98d48588b5c3bef4af2c01ec8a9a6a
surendra2chandu <surendra2chandu@gmail.com>
2025-01-28 16:00:31 +0530

    updates servie managers

--- src/retrival/DataRetrival.py
+++ src/retrival/DataRetrival.py
@@ -2,7 +2,6 @@
 from src.conf.Configurations import logger, NUMBER_OF_MATCHES
 from src.utilities.GetTokenEmbeddings import GetTokenEmbeddings
 from src.utilities.DataBaseUtility import DataBaseUtility
-from src.web_scraping.Temp2 import title
 
 
 class DataRetrival:
